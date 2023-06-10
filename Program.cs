b1b3f707662afaeec48acf9d3f6d9b62291bd656
Glenn <glennwiz@gmail.com>
2023-06-10 15:23:01 +0200

    added speed control and text,  and moved to .net6.0

--- Program.cs
+++ Program.cs
@@ -3,68 +3,98 @@ using SFML.Graphics;
 using SFML.Window;
 using SFML.System;
 
-namespace GameOfLifeSFML
-{    
-    public class Program
+public class Program
+{
+    private static Vector2f cellSize;
+    private static Time timePerFrame = Time.FromSeconds(0.5f);
+    private static Font font;
+    private static Text instructionText;
+
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        // Create a new SFML window
+        var mode = new VideoMode(800, 800);
+        var window = new RenderWindow(mode, "Game of Life");
+
+        // Handle the close events
+        window.Closed += (s, e) => window.Close();
+        window.KeyPressed += HandleKeyPress;
+
+        // Load the font
+        font = new Font("font/CaskaydiaCoveNerdFont-Bold.ttf");
+
+        // Initialize the instruction text
+        instructionText = new Text("Speed up - Key up\nSpeed down - Key down", font, 20)
         {
-            // Create a new SFML window
-            var mode = new VideoMode(800, 800);
-            var window = new RenderWindow(mode, "Game of Life");
+            FillColor = Color.White,
+            Position = new Vector2f(10, 10) // Position text in the top left corner
+        };
 
-            // Handle the close events
-            window.Closed += (s, e) => window.Close();
+        // Initialize the game
+        var game = new GameOfLife(30, 30);
+        cellSize = new Vector2f(mode.Width / game.Width, mode.Height / game.Height);
 
-            // Initialize the game
-            var game = new GameOfLife(30, 30);
+        // Create a clock for controlling the frame rate
+        var clock = new Clock();
 
-            // The size of each cell in pixels
-            var cellSize = new Vector2f(mode.Width / game.Width, mode.Height / game.Height);
+        // Run the game loop
+        while (window.IsOpen)
+        {
+            // Handle events
+            window.DispatchEvents();
 
-            // Create a clock for controlling the frame rate
-            var clock = new Clock();
-            var timePerFrame = Time.FromSeconds(0.5f); // Here you can adjust the speed of the game
-
-            // Run the game loop
-            while (window.IsOpen)
+            // Only update the game every timePerFrame seconds
+            if (clock.ElapsedTime >= timePerFrame)
             {
-                // Handle events
-                window.DispatchEvents();
+                // Reset the clock
+                clock.Restart();
 
-                // Only update the game every timePerFrame seconds
-                if (clock.ElapsedTime >= timePerFrame)
+                // Update the game
+                game.Update();
+
+                // Clear the window
+                window.Clear();
+
+                // Draw the current state of the grid
+                for (int y = 0; y < game.Height; y++)
                 {
-                    // Reset the clock
-                    clock.Restart();
-
-                    // Update the game
-                    game.Update();
-
-                    // Clear the window
-                    window.Clear();
-
-                    // Draw the current state of the grid
-                    for (int y = 0; y < game.Height; y++)
+                    for (int x = 0; x < game.Width; x++)
                     {
-                        for (int x = 0; x < game.Width; x++)
+                        if (game.Grid[x, y])
                         {
-                            if (game.Grid[x, y])
+                            var rectangle = new RectangleShape(cellSize)
                             {
-                                var rectangle = new RectangleShape(cellSize)
-                                {
-                                    FillColor = Color.White,
-                                    Position = new Vector2f(x * cellSize.X, y * cellSize.Y)
-                                };
-                                window.Draw(rectangle);
-                            }
+                                FillColor = Color.White,
+                                Position = new Vector2f(x * cellSize.X, y * cellSize.Y)
+                            };
+                            window.Draw(rectangle);
                         }
                     }
-
-                    // Display the window
-                    window.Display();
                 }
+
+                // Draw the instruction text
+                window.Draw(instructionText);
+
+                // Display the window
+                window.Display();
             }
+        }
+    }
+
+    private static void HandleKeyPress(object sender, KeyEventArgs e)
+    {
+        var window = (RenderWindow)sender;
+
+        switch (e.Code)
+        {
+            case Keyboard.Key.Up:
+                timePerFrame = Time.FromSeconds(timePerFrame.AsSeconds() - 0.1f);
+                break;
+            case Keyboard.Key.Down:
+                timePerFrame = Time.FromSeconds(timePerFrame.AsSeconds() + 0.1f);
+                break;
+            default:
+                break;
         }
     }
 }
